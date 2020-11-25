e77fed26c0963cec40a84c87da99b7601ebd39a3
DayongTong <37791502+DayongTong@users.noreply.github.com>
2020-11-25 00:45:32 -0500

    fixed plot.util.spline function
    now it outputs the correct plot with full length axes and correct units

--- plot_util.py
+++ plot_util.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_util(timesteps, x_sol, u_sol,n_x):
   fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(15,15))
@@ -19,26 +20,34 @@ def plot_util(timesteps, x_sol, u_sol,n_x):
 
   plt.tight_layout()
 
-def plot_util_spline(N, t_sol, x_s, u_s,n_x):
+def plot_util_spline(N, t_sol, x_s, u_s, n_x, r_min):
   fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(15,15))
   fig.subplots_adjust(hspace=10)
   fig.suptitle('Nominal State and Input Trajectories')
-  labels = ['Altitude $r$ (km)', r'Latitude $\alpha$ (deg)', r'Longitude $\beta$ (deg)',
+  labels = ['Altitude $r$ (m)', r'Latitude $\alpha$ (deg)', r'Longitude $\beta$ (deg)',
             '$V_x$ (m/s)', '$V_y$ (m/s)', '$V_z$ (m/s)',
             'Mass (kg)', r'Pitch $\phi$ (deg)', r'Yaw $\psi$ (deg)',
             'Thrust (N)', r'Pitch Rate $\omega_{\phi}$ (deg/s)', r'Yaw Rate $\omega_{\psi}$ (deg/s)']
-  
+  timevec = np.linspace(0,N*t_sol[0],500)
   for idx, ax in enumerate(axes.flatten()):
+    vals = []
     if idx < n_x:
-      vals = []
-      for i in range(int(N*t_sol[0])):
-        vals.append(x_s.value(i)[idx])
-        ax.plot(vals)
+      for i in timevec:
+        val = x_s.value(i)[idx]
+        if idx == 0:
+          val -= r_min
+          val /= 1000
+        elif idx == 1 or idx == 2 or idx == 7 or idx == 8:
+          val *= 180/np.pi
+        vals.append(val)
+      ax.plot(timevec,vals,'g')
     else:
-      vals = []
-      for i in range(int(N*t_sol[0])):
-        vals.append(u_s.value(i)[idx % n_x])
-        ax.plot(vals)
+      for i in timevec:
+        val = u_s.value(i)[idx % n_x]
+        if idx == 10 or idx == 11:
+          val *= 180/np.pi
+        vals.append(val)
+      ax.plot(timevec,vals,'g')
     ax.set(ylabel = labels[idx])
     ax.set(xlabel = "Simulation Time (s)")
 
