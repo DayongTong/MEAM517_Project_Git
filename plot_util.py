16b4ca0550245d05b82644b4dd5eb173cfe9cb10
DayongTong <37791502+DayongTong@users.noreply.github.com>
2020-12-02 11:51:56 -0500

    Update plot_util.py
    added plot_util_overlay so it can take in two trajectories

--- plot_util.py
+++ plot_util.py
@@ -20,6 +20,27 @@ def plot_util(timesteps, x_sol, u_sol,n_x):
 
   plt.tight_layout()
 
+def plot_util_overlay(timesteps1,x_reg,u_reg,timesteps2,x_dirtrel,u_dirtrel,n_x):
+  fig,axes = plt.subplots(nrows=4,ncols=3,figsize=(15,15))
+  fig.subplots_adjust(hspace=10)
+  fig.suptitle('Trajectories of simulation')
+  labels = ['Altitude $r$ (m)', r'Latitude $\alpha$ (deg)', r'Longitude $\beta$ (deg)',
+            '$V_x$ (m/s)', '$V_y$ (m/s)', '$V_z$ (m/s)',
+            'Mass (kg)', r'Pitch $\phi$ (deg)', r'Yaw $\psi$ (deg)',
+            'Thrust (N)', r'Pitch Rate $\omega_{\phi}$ (deg/s)', r'Yaw Rate $\omega_{\psi}$ (deg/s)']
+    
+  for idx,ax in enumerate(axes.flatten()):
+    if idx < n_x:
+      ax.plot(timesteps,x_sol[:,idx],label="DIRCON")
+      ax.plot(timesteps2,x_dirtrel[:,idx],label="DIRTREL")
+    else:
+      ax.plot(timesteps, u_sol[:,idx % n_x],label="DIRCON")
+      ax.plot(timesteps2,u_dirtrel[:,idx],label="DIRTREL")
+    ax.set(ylabel = labels[idx])
+    ax.set(xlabel = "Time (s)")
+    ax.legend(loc="lower right")
+
+
 def plot_util_spline(N, t_sol, x_s, u_s, n_x, r_min):
   fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(15,15))
   fig.subplots_adjust(hspace=10)
